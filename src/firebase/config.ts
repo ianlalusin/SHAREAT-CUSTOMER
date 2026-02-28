7fc64d9b316c25341f8720a6380a1718ba9b485e
Ian Lalusin <ianlalusin@gmail.com>
2026-02-28 05:51:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9137697931-43d46",
+  "appId": "1:920074442461:web:dc9c9241f81fe063e72d34",
+  "apiKey": "AIzaSyBzfs91EM9-7fi7p3p7EzKIWETd33xX8yY",
+  "authDomain": "studio-9137697931-43d46.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "920074442461"
+};
